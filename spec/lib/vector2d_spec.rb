005cf718de1d76d817bc79ae6a57e1c664976b76
Jack Christensen <jack@jackchristensen.com>
2013-03-16 21:35:31 -0500

    Use Vector2d#== in tests instead of components

--- spec/lib/vector2d_spec.rb
+++ spec/lib/vector2d_spec.rb
@@ -14,10 +14,7 @@ describe 'Vector2d' do
     it 'returns a new Vector2d with components added' do
       a = Vector2d.new(1, 2)
       b = Vector2d.new(3, 4)
-      result = a + b
-      expect(result).to be_kind_of(Vector2d)
-      expect(result.x).to eq 4
-      expect(result.y).to eq 6
+      expect(a + b).to eq Vector2d(4, 6)
     end
   end
 
