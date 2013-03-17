231098c23dae6b85ad58d77db86a2d411ccc8605
Jack Christensen <jack@jackchristensen.com>
2013-03-16 21:44:38 -0500

    Fix indentation

--- bin/space_champ_r.rb
+++ bin/space_champ_r.rb
@@ -9,16 +9,16 @@ require 'battle'
 
 class SpaceChampRWindow < Gosu::Window
   def initialize
-   super(640, 480, false)
-   self.caption = 'Space Champ R'
-   @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
-   @image = Gosu::Image.new self, 'resources/images/cruiser.bmp'
-   @ship_sprite = Gosu::Image.load_tiles self, 'resources/images/cruiser.bmp', 96, 96, false
-   @player_controller = Battle::PlayerController.new self
-   @ship = Battle::Ship.new renderer: Battle::ShipRenderer.new(@ship_sprite),
-    position: Vector2d.new(0, 0),
-    direction: 0,
-    controller: @player_controller
+    super(640, 480, false)
+    self.caption = 'Space Champ R'
+    @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    @image = Gosu::Image.new self, 'resources/images/cruiser.bmp'
+    @ship_sprite = Gosu::Image.load_tiles self, 'resources/images/cruiser.bmp', 96, 96, false
+    @player_controller = Battle::PlayerController.new self
+    @ship = Battle::Ship.new renderer: Battle::ShipRenderer.new(@ship_sprite),
+      position: Vector2d.new(0, 0),
+      direction: 0,
+      controller: @player_controller
   end
 
   def update
